474cfb427cd17bf48f3e4532da7069ecce7a7065
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-22 13:38:13 -0800

    Created api routes folder for user.js, index.js and userRoutes.js also added server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+});
