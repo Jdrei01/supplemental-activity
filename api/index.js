2a1f59d046f92eb582659b85b5595d580f9f6cdc
Jandrei Timoteo <jandrei_tim@yahoo.com>
2024-01-22 13:55:06 -0800

    Update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
 router.use('/api/users', userRoutes);
 
 module.exports = router;
